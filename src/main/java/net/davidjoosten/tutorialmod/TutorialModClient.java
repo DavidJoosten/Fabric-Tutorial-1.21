dcf2df9ad2876138f291c907cd9ed635d6a2a838
DavidJoosten <david@dejoostens.nl>
2026-02-09 19:47:07 +0100

    upload

---
+++ src/main/java/net/davidjoosten/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.davidjoosten.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
